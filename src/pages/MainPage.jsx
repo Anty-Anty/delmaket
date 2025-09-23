83a1914620d90afb01f9475c5b5cde00de15ad87
Anton <ibeliev@gmail.com>
2025-09-23 07:35:44 -0700

    MainPage.jsx added

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+const MainPage = () => {
+
+    return (
+        <>
+            <div className="logo">delmaket</div>
+            <div className="links">макеты о мастерской контакты</div>
+            <div className="first-s-main-photo"></div>
+            <div className="second-s-info"></div>
+            <div className="third-s-works"></div>
+            <div className="fourth-s-clients"></div>
+            <div className="footer"></div>
+            
+        </>
+    )
+};
+
+export default MainPage;
