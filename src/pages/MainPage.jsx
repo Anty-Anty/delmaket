70542b72f9ba28eb892955eeae2a000005d2b347
Anty-Anty <ibeliev@gmail.com>
2025-09-24 12:52:06 -0700

    added modal and backdrop

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+
 import Footer from "../components/Footer";
+import Modal from "../components/UIElements/Modal";
 
 import "./MainPage.css"
 
 const MainPage = () => {
+
+    const [showModal, setShowModal] = useState(false);
+
+        const closeModalHandler = () => {
+        setShowModal(false);
+    };
 
     return (
         <>
@@ -17,6 +26,19 @@ const MainPage = () => {
             </div>
 
             <div id="home" className="main-photo"></div>
+
+            <button onClick={() => setShowModal(true)}>Open Modal</button>
+
+            <Modal
+                show={showModal}
+                onCancel={() => closeModalHandler}
+                footer={<button onClick={closeModalHandler}>Close</button>}
+            >
+                <p>This is some content inside the modal.</p>
+                <p>You can put text, forms, or even components here.</p>
+                <div className="modal-works">h</div>
+            </Modal>
+
             <div className="info"></div>
             <div id="models" className="works"></div>
             <div id="about" className="about"></div>
